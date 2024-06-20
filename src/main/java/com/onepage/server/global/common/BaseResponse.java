20c8d1ed5d0140545460c3fb80c3e1ab381c4f31
최미래 <miraechoi@dgsw.hs.kr>
2024-06-20 11:32:37 +0900

    feat :: base response 생성

---
+++ src/main/java/com/onepage/server/global/common/BaseResponse.java
@@ -0,0 +1,25 @@
+package com.onepage.server.global.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class BaseResponse {
+
+    private HttpStatus httpStatus;
+    private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Object data;
+
+    public BaseResponse(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    public BaseResponse(HttpStatus httpStatus, String message, Object data){
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.data = data;
+    }
+}
