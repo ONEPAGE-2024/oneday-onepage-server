de51691ec9d6695d8e43da78c239e2be2d336c1c
최미래 <miraechoi@dgsw.hs.kr>
2024-06-20 15:19:24 +0900

    feat :: user repository 생성

---
+++ src/main/java/com/onepage/server/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.onepage.server.domain.user.repository;
+
+import com.onepage.server.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+    Optional<User> findByEmailEquals(String email);
+}
