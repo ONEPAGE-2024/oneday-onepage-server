05dd27c792b072ca43013dee90e314ce8f762a25
최미래 <miraechoi@dgsw.hs.kr>
2024-06-20 13:40:30 +0900

    feat :: /diary/update api 생성

--- src/main/java/com/onepage/server/domain/controller/DiaryController.java
+++ src/main/java/com/onepage/server/domain/controller/DiaryController.java
@@ -22,4 +22,8 @@ public class DiaryController {
         return diaryService.findAll();
     }
 
+    @PutMapping("/update")
+    public void updateDiary(@RequestBody DiaryDTO diaryDTO) {
+        diaryService.updateDiary(diaryDTO);
+    }
 }
